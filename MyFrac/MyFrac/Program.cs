88bf61b1ba01a82a7cfa45ef88635a773dc07dcc
Roman <manko.roman1123@vu.cdu.edu.ua>
2024-10-21 11:17:33 +0300

    constructor & GCD

---
+++ MyFrac/MyFrac/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class MyFrac
+{
+    //поля для чисельника та знаменника
+    public long Numerator { get; private set; } //можна напряму брати, неможна напряму міняти
+    public long Denominator { get; private set; }
+
+    //конструктор
+    public MyFrac(long numerator, long denominator)
+    {
+        if (denominator == 0)
+        {
+            throw new ArgumentException("Знаменник не може дорівнювати нулю.");
+        }
+
+        //якщо знаменник від'ємний - змінюємо знаки чисельника + знаменника
+        if (denominator < 0)
+        {
+            numerator = -numerator;
+            denominator = -denominator;
+        }
+
+        //скорочуємо дріб
+        long gcd = GCD(Math.Abs(numerator), Math.Abs(denominator));
+        Numerator = numerator / gcd;
+        Denominator = denominator / gcd;
+    }
+
+    //алгоритм Евкліда для знаходження НСД(GCD)
+    private long GCD(long a, long b)
+    {
+        while (b != 0)
+        {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
